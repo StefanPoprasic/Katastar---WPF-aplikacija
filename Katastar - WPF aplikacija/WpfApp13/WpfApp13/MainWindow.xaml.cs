59cf1f5ab29950589ff79f4b55fce98957ebbc5f
StefanPoprasic <47986391+StefanPoprasic@users.noreply.github.com>
2020-11-07 20:55:34 +0100

    Initial commit

---
+++ Katastar - WPF aplikacija/WpfApp13/WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KatastarDataContext Katastar = new KatastarDataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void puniCombo() // Data entry in combobox (name of municipalities)
+        {
+            var opstine = from o in Katastar.KatastarskeOpstines
+                          select o;
+            cbKatOpstine.ItemsSource = opstine;
+        }
+        private void puniDatagrid() // Data entry in the datagrid for the name of the municipality selected in cobobox
+        {
+            var parcele = from p in Katastar.Parceles
+                          where p.IDKatOpstine == int.Parse(((KatastarskeOpstine)cbKatOpstine.SelectedValue).IDKatOpstina.ToString())
+                          select p;
+            datagrid.ItemsSource = parcele;
+            // in MainWindow.xaml was made what data to show
+        }
+
+
+        private void Datagrid_LoadingRowDetails(object sender, DataGridRowDetailsEventArgs e)
+        {
+           
+           // view of objects located on the land that we clicked, there can be only one object on that land
+            try
+            {
+                Objekti objekti = (from o in Katastar.Objektis
+                                   where o.IDKatOpstine == int.Parse(((Parcele)datagrid.SelectedValue).IDKatOpstine.ToString()) && o.IDParcele == int.Parse(((Parcele)datagrid.SelectedValue).IDParcele.ToString())
+                                   select o).First();
+                if (objekti != null)
+                {
+
+                    TextBox tb1 = e.DetailsElement.FindName("txtIDObjekta") as TextBox;
+                    tb1.Text = objekti.IDObjekta.ToString();
+                    TextBox tb2 = e.DetailsElement.FindName("txtVlasnik") as TextBox;
+                    tb2.Text = objekti.Vlasnik;
+                    TextBox tb3 = e.DetailsElement.FindName("txtKvadratura") as TextBox;
+                    tb3.Text = objekti.Kvadratura.ToString();
+                    CheckBox cb1 = e.DetailsElement.FindName("cbUknjizeno") as CheckBox;
+                    if (objekti.Uknjizeno == true) cb1.IsChecked = true;
+
+                }
+            }
+            catch(Exception)
+            {
+                TextBox tb1 = e.DetailsElement.FindName("txtIDObjekta") as TextBox;
+                tb1.Text = "";
+                TextBox tb2 = e.DetailsElement.FindName("txtVlasnik") as TextBox;
+                tb2.Text = "";
+                TextBox tb3 = e.DetailsElement.FindName("txtKvadratura") as TextBox;
+                tb3.Text = "";
+                CheckBox cb1 = e.DetailsElement.FindName("cbUknjizeno") as CheckBox;
+                cb1.IsChecked = false;
+                MessageBox.Show("Nema objekta na parceli");
+            }
+           
+            
+  
+        }
+
+        private void Brisanje(object sender, RoutedEventArgs e)
+        {
+
+            // deleting an object by right - clicking on a specific plot in the datagrid
+
+            Objekti brisi = (from o in Katastar.Objektis
+                               where o.IDKatOpstine == int.Parse(((Parcele)datagrid.SelectedValue).IDKatOpstine.ToString()) && o.IDParcele == int.Parse(((Parcele)datagrid.SelectedValue).IDParcele.ToString())
+                               select o).First();
+            MessageBoxResult rez = MessageBox.Show("Da li ste sigurni da zelite da obrisete?", "Brisanje", MessageBoxButton.YesNo);
+            if (rez == MessageBoxResult.Yes)
+            {
+                Katastar.Objektis.DeleteOnSubmit(brisi);
+                
+                try
+                {
+                    Katastar.SubmitChanges();
+                    // Resetting dataagrids and fields that shows us unbooked objects for the selected municipality
+                    puniDatagrid();
+                    neuknjizeni();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Ne moze da se obrise !" + ex);
+                }
+            }
+        }
+
+        private void CbKatOpstine_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            puniDatagrid();
+            neuknjizeni();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            puniCombo();
+        }
+        private string vratiVlasnistvo()
+        {
+            string pom = "";
+            foreach(RadioButton rb in gridVlasnistvo.Children)
+            {
+                if(rb.IsChecked==true)
+                pom = rb.Content.ToString();
+            }
+            return pom;
+        }
+        private void TbSifraParcele_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbSifraParcele.Text))
+            {
+                gbVlasnistvo.IsEnabled = true;
+            }
+        }
+
+        private void BtnIzmeni_Click(object sender, RoutedEventArgs e)
+        {
+            string vlasnistvo = vratiVlasnistvo();
+            try
+            {
+                if (!String.IsNullOrEmpty(cbKatOpstine.Text) && !String.IsNullOrEmpty(tbSifraParcele.Text) && !String.IsNullOrEmpty(vlasnistvo))
+                {
+                    Parcele parcele = (from p in Katastar.Parceles
+                                       where p.IDKatOpstine == int.Parse(((KatastarskeOpstine)cbKatOpstine.SelectedValue).IDKatOpstina.ToString()) && p.IDParcele == int.Parse(tbSifraParcele.Text)
+                                       select p).Single();
+                    parcele.Vlasnistvo = vlasnistvo;
+                    try
+                    {
+                        Katastar.SubmitChanges();
+                        MessageBox.Show("Vlasnistvo za parcelu " + tbSifraParcele.Text + " u katastarskoj opstini " + ((KatastarskeOpstine)cbKatOpstine.SelectedValue).Naziv + " je uspesno promenjena");
+                        puniDatagrid();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Neuspesno " + ex);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nesto nije popunjeno");
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Nije dobro uneta sifra");
+            }
+        }
+        private void neuknjizeni()
+        {
+            
+            var objekti = (from o in Katastar.Objektis
+                           where o.IDKatOpstine == int.Parse(((KatastarskeOpstine)cbKatOpstine.SelectedValue).IDKatOpstina.ToString())&& (o.Uknjizeno==null || o.Uknjizeno==false)
+                           select o).Count();
+            tbNeuknjizeni.Text = objekti.ToString();
+        }
+
+        private void Unos(object sender, RoutedEventArgs e)
+        {
+            UnosNovog nov = new UnosNovog();
+            nov.ShowDialog();
+            
+
+        }
+    }
+}
---
+++ Katastar - WPF aplikacija/WpfApp13/WpfApp13/UnosNovog.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for UnosNovog.xaml
+    /// </summary>
+    public partial class UnosNovog : Window
+    {
+        KatastarDataContext Katastar = new KatastarDataContext();
+        
+        public UnosNovog()
+        {
+            
+            InitializeComponent();
+            puniComb();
+        }
+        private void puniComb()
+        {
+            var opstine = from o in Katastar.KatastarskeOpstines
+                          select o;
+            cbKatOpstina.ItemsSource = opstine;
+        }
+        private void puniListu()
+        {
+            var parcele =from p in Katastar.Parceles 
+                where p.IDKatOpstine == int.Parse(((KatastarskeOpstine)cbKatOpstina.SelectedValue).IDKatOpstina.ToString())
+                select p;
+            lbParcela.ItemsSource = parcele;
+        }
+
+        private void CbKatOpstina_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+                    puniListu();
+            
+        }
+
+        private void BtnPotvrdi_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbSifra.Text) && !String.IsNullOrEmpty(tbVlasnik.Text) && !String.IsNullOrEmpty(tbKvadratura.Text) && !String.IsNullOrEmpty(cbKatOpstina.Text) && lbParcela.SelectedValue != null)
+            {
+                try
+                {
+                    Objekti objekti = (from o in Katastar.Objektis
+                                       where o.IDKatOpstine == int.Parse(((Parcele)lbParcela.SelectedValue).IDKatOpstine.ToString()) && o.IDParcele == int.Parse(((Parcele)lbParcela.SelectedValue).IDParcele.ToString())
+                                       select o).First();
+                    if (objekti != null)
+                    {
+                        MessageBox.Show("Na ovoj parceli ima objekta pokusaj na drugoj");
+                    }
+                }
+                catch(Exception)
+                {
+                    Objekti nov = new Objekti()
+                        {
+                            IDObjekta = (int.Parse)(tbSifra.Text),
+                            Vlasnik = tbVlasnik.Text,
+                            Kvadratura = int.Parse(tbKvadratura.Text),
+                            Uknjizeno = (bool)cbUknjizeno.IsChecked,
+                            IDKatOpstine = (int.Parse)(((KatastarskeOpstine)cbKatOpstina.SelectedValue).IDKatOpstina.ToString()),
+                            IDParcele = (int.Parse)(((Parcele)lbParcela.SelectedValue).IDParcele.ToString()),
+                        };
+
+
+                        Katastar.Objektis.InsertOnSubmit(nov);
+                       
+
+
+                        try
+                        {
+                            Katastar.SubmitChanges();
+                            MessageBox.Show("Uspesan unos");
+                            resetuj();
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sva polja su obavezna!!!");
+            }
+        }
+
+        private void BtnOdustani_Click(object sender, RoutedEventArgs e)
+        {
+            resetuj();
+        }
+        private void resetuj()
+        {
+            tbSifra.Text = "";
+            tbVlasnik.Text = "";
+            tbKvadratura.Text = "";
+            cbUknjizeno.IsChecked = false;
+            cbKatOpstina.SelectedIndex = -1;
+            lbParcela.SelectedIndex = -1;
+        }
+    }
+}
